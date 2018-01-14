fb27859bb01934b94065c6de4ce03d9747096365
Eric Huss <eric@huss.org>
2018-01-14 15:50:06 -0800

    Add padding, and remember last input.

--- insert_numbers.py
+++ insert_numbers.py
@@ -2,16 +2,21 @@ import sublime
 import sublime_plugin
 
 
+last_input = '1 '
+
+
 class InsertNumbersCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        default = '1 '
+        # s = self.view.sel()
         # if len(s) > 1:
         #     default = '1 %i' % (len(s),)
         self.view.window().show_input_panel(
-            'Enter Start [Stop [Step]]', default, self.on_done, None, None)
+            'Enter Start [Stop [Step]]', last_input, self.on_done, None, None)
 
     def on_done(self, text):
+        global last_input
+        last_input = text
         self.view.run_command('insert_numbers_act', {'text': text})
 
 
@@ -23,20 +28,25 @@ class InsertNumbersAct(sublime_plugin.TextCommand):
         step = 1
         try:
             start = int(parts[0])
+            width = len(parts[0])
             if len(parts) > 1:
                 stop = int(parts[1])
+                width = min(len(parts[1]), len(parts[0]))
                 if len(parts) > 2:
                     step = int(parts[2])
         except ValueError:
             sublime.error_message('Values must be numbers.')
             return
 
+        def pad_i(i):
+            return '%0*i' % (width, i)
+
         if stop is None:
             i = start
             for r in self.view.sel():
-                self.view.insert(edit, r.begin(), str(i))
+                self.view.insert(edit, r.begin(), pad_i(i))
                 i += step
         else:
-            text = '\n'.join(map(str, range(start, stop + 1, step)))
+            text = '\n'.join(map(pad_i, range(start, stop + 1, step)))
             for r in self.view.sel():
                 self.view.insert(edit, r.begin(), text)
